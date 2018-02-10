b8dbbbb7735e8dfad561605dd8973d76d3af704f
Michael Dalton <modalton@usc.edu>
2018-02-10 10:15:53 -0800

    morning git sync

--- src/client/components/CommentFeed.jsx
+++ src/client/components/CommentFeed.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import Paper from 'material-ui/Paper';
+
+const Comment = (props) => {
+  console.log('comment daty',props.datum)
+  return (
+    <Paper>
+      <img src={`uploads/${props.datum.img}`} style={{height:100}}/>
+      {
+        props.datum.caption
+      }
+    </Paper>
+  )
+}
 
 
+const CommentFeed = (props) => {
+  
+//  let arr = props.comments.map(c => <Comment datum={c}/>)
+  
+  return (
+    <div>
+      {
+        props.comments.map(c => <Comment datum={c}/>)
+    }
+    </div>
+  )
+}
 
 export default CommentFeed;
