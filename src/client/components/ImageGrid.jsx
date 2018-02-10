1e269e6875d93e0d4d012c964a45aea40c330fe0
Mejin Leechor <amleechor@gmail.com>
2018-02-10 13:39:24 -0800

    Reset master

--- src/client/components/ImageGrid.jsx
+++ src/client/components/ImageGrid.jsx
@@ -3,6 +3,7 @@ import { GridList, GridTile } from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 // import HomePageContainer from './HomePageContainer.jsx';
 
 const styles = {
@@ -38,15 +39,16 @@ class ImageGrid extends Component {
                     style={styles.gridList} cols={3}
                 >
                     <Subheader>Stories</Subheader>
-                    {this.props.postList.map((tile) => (
-                        <GridTile
-                            key={tile.img}
-                            title={tile.title}
-                            subtitle={<span>by <b>{tile.user_id}</b></span>}
-                            actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+                    {this.props.postList.map((post) => (
+                        <Link to={'/commentfeed/'+post.post_id}><GridTile
+                            key={post.created_at}
+                            title={post.title}
+                            post_id={post.post_id}
+                            // actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+                            // onClick={() => this.props.routeToPost(post.post_id)}
                         >
-                            <img src={tile.img} />
-                        </GridTile>
+                            <img src={post.img} />
+                        </GridTile></Link>
                     ))}
                 </GridList>
             </div>
