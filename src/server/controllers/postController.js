f15253ee3b7636a7f1c430374b099bcef8b0b410
Michael Dalton <modalton@usc.edu>
2018-02-09 16:33:38 -0800

    CR of CRUD on postcontroller

--- src/server/controllers/postController.js
+++ src/server/controllers/postController.js
@@ -2,20 +2,30 @@ let sql = require('../config.js');
 let sanatize = require('sqlstring');
 module.exports = {
 
-  find : function(req,res,next){
-    let { user_id } = req.body
-    sql.query(sanatize.format('SELECT * FROM user WHERE id=$',[user_id]), function (error, results, fields) {
-      if (error) res.send(error);
-      console.log('The solution s: ', results[0].solution);
-      res.send(results)
+  create : function(req,res,next){
+    const {user_id,title,link,img} = req.body;
+    sql.query(sanatize.format('INSERT INTO post (user_id,title,link,img) VALUES (?,?,?,?)',[user_id,title,link,img]),function(err,results,fields){
+      if (err) res.send(err);
+      console.log(fields);
+      res.send(results);
     });
-
+    
+  },
+  
+  read : function(req,res,next){
+    const { user_id } = req.body;
+    
+    sql.query(sanatize.format('SELECT id,username,email FROM user WHERE id=?',[user_id]), function (err, results, fields) {
+      if (err) res.send(err);
+      res.send(results);
+    });
+    
   },
 
   end : function(req,res,next){
 
     sql.end()
-    res.send('closed')
+    res.json('closed')
   }
  
 
