6bbd37b8349da32dca3c8aadb1a7b484fa99947b
saltyandsmiling <eric.rudolph.carrillo@gmail.com>
2018-02-14 10:40:51 -0800

    working front end

--- src/client/containers/HomePageContainer.jsx
+++ src/client/containers/HomePageContainer.jsx
@@ -5,47 +5,42 @@ import PostCreator from '../components/PostCreator.jsx';
 
 class HomePageContainer extends Component {
     constructor(props) {
-        super(props);
+      super(props);
         this.state = {};
         this.state.postList = [];
 
         this.renderPosts = this.renderPosts.bind(this);
         this.submitPost = this.submitPost.bind(this);
-        // this.routeToPost = this.routeToPost.bind(this);
     };
 
     renderPosts(data) {
-        const stateCopy = {};
-        stateCopy.postList = data;
-        this.setState({ postList: stateCopy.postList });
+      const stateCopy = {};
+      stateCopy.postList = data;
+      this.setState({ postList: stateCopy.postList });
     }
 
-    // routeToPost(post_id){
-    //     console.log('gonna route you to post ' + post_id);
-    // }
-
     submitPost(data){
-        fetch('/newPost', {
-            method: 'POST',
-            body: data
-        })
-        .then(res => res.json())
-        .then((res) => this.renderPosts(res));
+      fetch('/newPost', {
+        method: 'POST',
+        body: data
+      })
+      .then(res => res.json())
+      .then((res) => this.renderPosts(res));
     }
 
     render() {
-        return (
-            <div>
-                <NavBar />
-                <ImageGrid
-                    postList={this.state.postList}
-                    renderPosts={this.renderPosts}
-                // routeToPost={this.routeToPost}
-                />
-                <PostCreator
-                    submitPost={this.submitPost} />
-            </div>
-        );
+      return (
+        <div>
+          <NavBar />
+          <ImageGrid
+            postList={this.state.postList}
+            renderPosts={this.renderPosts}
+          />
+          <PostCreator
+            submitPost={this.submitPost}
+          />
+        </div>
+      );
     }
 }
 
--- src/client/containers/StoryCommentPageContainer.jsx
+++ src/client/containers/StoryCommentPageContainer.jsx
@@ -5,37 +5,38 @@ import CreateComment from '../components/CreateComment.jsx';
 
 export default class StoryCommentPageContainer extends Component{
 
-  //can;t super props here for some reason
   constructor(props){
-    super()
-    this.state = {comments:[], post:null, post_id:parseInt(props.match.params.post_id)}
-    console.log(props.match.params.post_id);
+    super();
+    this.state = {
+      comments: [],
+      post: null,
+      post_id: parseInt(props.match.params.post_id)
+    }
   }
 
   componentDidMount(){
-    fetch(`/${this.state.post_id}`).then(res => res.json())
+    fetch(`/${this.state.post_id}`)
+      .then(res => res.json())
       .then(data => {
-        this.setState({comments:data})
-      })
+        this.setState({ comments: data })
+      });
 
-    fetch(`/post/${this.state.post_id}`).then(res => res.json())
+    fetch(`/post/${this.state.post_id}`)
+      .then(res => res.json())
       .then(data => {
-        console.log('hemmo',data)
         this.setState({post:data})
       })
   }
-
-
 
   render(){
     return (
       <div>
         { (this.state.post && this.state.comments) &&
           <div>
-              <StoryHeader post = {this.state.post}/>
-                <CommentFeed comments={this.state.comments}/>
-                  <CreateComment dataInject={{user_id:1,post_id:this.state.post_id}}/>
-            </div>
+            <StoryHeader post = {this.state.post}/>
+              <CommentFeed comments={this.state.comments}/>
+                <CreateComment dataInject={{user_id:11,post_id:this.state.post_id}}/>
+          </div>
         }
       </div>
     )
